1db7d1a85135a2bf3717290dbf280934977c1f48
Piotr Mankowski <pmankowski@partners.org>
2013-12-02 16:49:11 -0500

    Loader testing

--- spec/lib/modules/etl/transform/psq_merger_spec.rb
+++ spec/lib/modules/etl/transform/psq_merger_spec.rb
@@ -22,16 +22,14 @@ describe ETL::PsqMerger do
     it "should merge all tabs in the template source into a master xls (csv?) file" do
       # 8 subjects * 23 SP + 1 subject * 12 SP
 
+      File.delete destination_file_path
 
       psq_merger = ETL::PsqMerger.new subject_group, source_file_list, destination_file_path
 
       expect(psq_merger.merge_files).to be_true
 
-      
-
-
-
-
+      expect(File.exists? destination_file_path).to be_true
+      expect(File.open(destination_file_path).readlines.size).to eq((8 * 23) + (1 * 12))
     end
 
   end
