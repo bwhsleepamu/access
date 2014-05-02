64a3ec869f3f73cc3dc79098be64e0dd50c72df9
Piotr Mankowski <pmankowski@partners.org>
2014-05-01 20:15:16 -0400

    Sleep Stage Loader update

--- lib/modules/etl/load/sleep_stage_loader.rb
+++ lib/modules/etl/load/sleep_stage_loader.rb
@@ -13,12 +13,16 @@ module ETL
 
 
     def initialize(root_path, subject_group, source_type, documentation, user)
+      LOAD_LOG.info "# Initializing Sleep Stage Loader for subject group #{subject_group.name}"
+
       event_record = EventDictionary.includes(:data_dictionary => :data_type).find_by_name(EVENT_NAME)
       raise StandardError, "Cannot find record in event dictionary: #{EVENT_NAME}" unless event_record.present?
 
-      @subject_list = []
+      @subject_group = subject_group
+      @init_subject_list = []
+      failed_subject_list = []
 
-      subject_group.each do |subject|
+      @subject_group.each do |subject|
 
 
         begin
@@ -33,85 +37,40 @@ module ETL
             path: source.location,
             skip_lines: 0
           }
+
+          db_loader = ETL::DatabaseLoader.new(data_info, object_map(subject), column_map, source, documentation, subject)
+
+          @init_subject_list << {subject: subject, loader: db_loader}
+        rescue => error
+          LOAD_LOG.error "## Failed to initialize subject #{subject.subject_code}"
+          LOAD_LOG.info "## Error: #{error.message}\n## Backtrace:\n#{error.backtrace}\n\n"
+
+          failed_subject_list << subject.subject_code
         end
 
-      end
+        LOAD_LOG.info "Finished initializing Sleep Stage Loader\n\nSubjects successfully initialized:\n#{@init_subject_list.map{|s| s[:subject].subject_code}}\n\nSubjects failing to initialize: #{failed_subject_list}"
 
-
-
-
-      begin
-        #xls_file_name = "#{subject_code}#{@@xls_file_pattern}"
-
-        # csv_file_name = "#{subject_code}#{@@csv_file_pattern}"
-
-        (@subject = Subject.find_by(subject_code: subject_code))
-
-        patient_year = get_year(File.join(parent_path, subject_code, "Sleep", xls_file_name))
-
-        source_type = SourceType.find_by_name(@@source_type_name)
-        user = User.find_by_email(@@user_email)
-        documentation = Documentation.find_by_title(@@doc_title)
-        event_dictionary = EventDictionary.includes(:data_dictionary => :data_type).find_by_name(@@event_name)
-        source = Source.create(
-          source_type_id: source_type.id, 
-          user_id: user.id, 
-          location: File.join(general_path, subject_code, "Sleep", csv_file_name), 
-          notes: "Year taken from #{subject_code}Sleep.xls file." 
-        )
-
-        data_info = {
-          path: File.join(parent_path, subject_code, "Sleep", csv_file_name),
-          skip_lines: 0
-        }
-
-        column_map = [
-          {
-            target: :subject,
-            field: :subject_code,
-            definitive: true
-          },
-          {
-            target: :datum,
-            event_name: @@event_name,
-            field: :sleep_wake_period
-          },
-          {
-            target: :event,
-            event_name: @@event_name,
-            field: :labtime_decimal
-          },
-          {
-            target: :datum,
-            event_name: @@event_name,
-            field: :scored_stage
-          }
-        ]
-
-        object_map =
-
-        @db_loader = ETL::DatabaseLoader.new(data_info, object_map, column_map, source, documentation, @subject)
-        LOAD_LOG.info "#### Successfully initialized #{@subject.subject_code} for loading of sleep stage data."
-
-        @valid = true
-      rescue => error
-        LOAD_LOG.info "#### Setup Error: #{error.message}\n\nBacktrace:\n#{error.backtrace}"
-        @valid = false
       end
     end
 
-    def load_subject
-      if @valid and Event.current.where(subject_id: @subject.id, name: 'scored_epoch').count == 0
-        loaded = false
+    def load
+      loaded_subjects = []
+      failed_subjects = []
+
+      LOAD_LOG.info "# Starting to load subjects for subject group #{@subject_group.name}"
+
+      @init_subject_list.each do |subject_info|
         begin
-          LOAD_LOG.info "###################### Starting #{@subject.subject_code} #######################"
-          loaded = @db_loader.load_data
+          LOAD_LOG.info "## Loading subject #{subject_info[:subject].subject_code}"
+          if subject_info[:loader].load_data
+            loaded_subjects << subject_info[:subject].subject_code
+          else
+            raise StandardError, "Loading of subject #{subject_info[:subject].subject_code} failed!"
+          end
         rescue => error
-          LOAD_LOG.info "#### Load Error: #{error.message}\n\nBacktrace:\n#{error.backtrace}\n\n"
+          failed_subjects << subject_info[:subject].subject_code
+          LOAD_LOG.info "## Sleep Stage Loading Error for subject #{subject_info[:subject].subject_code}!\n#{error.message}\nBacktrace:\n#{error.backtrace}\n\n"
         end
-        loaded
-      else
-        false
       end
     end
  
@@ -167,30 +126,6 @@ module ETL
         raise StandardError, "Could not find sleep file for subject #{subject.subject_code}. Looked in: #{perspective_path}"
       end
     end
-
-    def get_year(path)
-      if @subject.admit_date.present?
-        @subject.admit_date.year
-      else
-
-        raise StandardError, "Could not find year!"
-        # xx = Roo::Excel.new(path)
-        # xx.default_sheet = xx.sheets[0]
-        #
-        # #xx.default_sheet = "tasciifiles"
-        #
-        # row_count = 2
-        # until (m = /.*_*.(\d\d)_.*/i.match(xx.row(row_count)[0])).present? or row_count > 100
-        #   row_count += 1
-        # end
-        #
-        # raise StandardError, "Could not find suitable year from excel file!" if row_count > 100
-        #
-        # year = m[1].to_i
-        # year + ((year < 60) ? 2000 : 1900)
-      end
-    end
-
   end
 end
 
